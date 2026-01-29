96d80a0c725d757fdd5567cf6cda290ea282c45c
LidXo <lidxo.dev@gmail.com>
2026-01-29 20:17:30 +0000

    Initial commit

---
+++ projet1.py
@@ -0,0 +1,17 @@
+# print('hello world!')
+
+# COMMENTAIRES
+
+# width = 20
+#height = 5 * 9
+#C = width * height
+#print(C)
+
+tax = 12.5 / 100
+price = 100.50
+price * tax
+price + _
+round(_, 2)
+
+
+
