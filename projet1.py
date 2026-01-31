5eebcaaf93a7694eacba8800448327d1ecd5253c
LidXo <lidxo.dev@gmail.com>
2026-01-31 23:33:33 +0000

    apprentissage continue

--- projet1.py
+++ projet1.py
@@ -1,16 +1,103 @@
-# print('hello world!')
+"""print('hello world!')
+print(100)
+print(3.14)"""
+from traceback import print_tb
 
-# COMMENTAIRES
+"""20 + 32
+5 ** 2
+(5 + 9) * (15 - 7)
+print(7 +5)"""
 
-# width = 20
-#height = 5 * 9
-#C = width * height
-#print(C)
+"""print(9 / 5)
+print(5 / 9)
+print(9 // 5)"""
 
-tax = 12.5 / 100
-price = 100.50
-price * tax
-price + _
-round(_, 2)
+'''print(7.0 / 3.0)
+print(7.0 // 3.0) # // affiche l'entier naturel le plus proche
+print(7.0 % 3.0)'''  # % affiche le reste de la division
+
+'''print(2 ** 3 ** 2)
+print((2 ** 3) ** 2) # c'est une question de priorité
+print(16 - 2 * 5 // 3 + 1)'''
+
+# APPEL DE FONCTION
+# Carré
+'''def square(x):
+    return x ** 2
+print(square(3))
+square(5) # Ne marche pas
+
+# Soustraction
+def sub(x, y):
+    return x - y
+print(sub(9, 6))
+print(sub(17, 4))'''
+
+# DATA TYPES
+'''print(type("Hello World"))
+print(type(17))
+print("Hello, World")
+print(type(3.2))'''
+
+'''print(type("3.14"))
+print(type("17"))
+# Tant que les valeurs sont dans '' ou "" ou """""" ou '''''', le type sera toujours STRING
+print('''"Oh No", '''she exclaimed''', "Ben's bike is broken" ''')
+print("""This message will span
+several lines
+of the text""") # de cette manière pour revenir à la ligne
+print('This is a string')
+print("""And so is this.""")'''
+
+print(42500)
+print(42,500)
+print(42, 17, 56, 11, 45, 63)
+print(3.4, "Hello", 45)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
